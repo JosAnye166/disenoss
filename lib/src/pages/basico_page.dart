80a395ffe1d6f184839ef5cc9ed0ca1b14ab2164
JosAnye166 <josabor33@gmail.com>
2022-02-25 22:09:06 -0500

    primer commit

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -2,12 +2,50 @@ import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
 
+final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+final estiloSubTitulo = TextStyle(fontSize: 20.0, color: Colors.grey);
+
+
+
+
 //Inicial
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Basico Page')
-      ),
+      body: Column(
+        children: <Widget>[
+
+          Image(
+            image: NetworkImage('https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879__340.jpg')
+          ),
+
+           Container(
+             padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+             child: Row(
+               children: <Widget> [
+
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      Text('Lago en lugar frio', style: estiloTitulo),
+                      SizedBox( height: 7.0),
+                      Text('Lago en las montañas', style: estiloSubTitulo),
+                    ],
+                  ),
+                ),
+
+                Icon(Icons.star, color: Colors.red, size: 30.0,),
+                Text('41', style: TextStyle( fontSize: 20.0)),
+
+
+
+               ],
+              ),
+           )
+
+        ],
+      )
     );
   }
 }
